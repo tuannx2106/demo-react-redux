9854bf131343c8c5863c716d6fa22fafce6da615
tuannx21 <tuannguyen@DESKTOP-1E1AV4K.localdomain>
2019-12-17 20:35:14 +0700

    update

--- src/App.js
+++ src/App.js
@@ -75,16 +75,16 @@ function App() {
                   <h3>BASIC INFO</h3>
                   <div className="form-group">
                     <label className="form-label" htmlFor="input1">Name</label>
-                    <input type="text" name="input1" placeholder="Name" className="form-control" />
+                    <input type="text" name="input1" placeholder="Name" className="form-control w-full" />
                   </div>
                   <div className="form-group">
                     <label className="form-label" htmlFor="input2">Address</label>
                     <div className="form-inline">
-                      <input type="text" name="input2" className="form-control" />
-                      <select className="form-control">
+                      <input type="text" name="input2" className="form-control form-inline-item" />
+                      <select className="form-control form-inline-item">
                         <option>1</option>
                       </select>
-                      <select className="form-control">
+                      <select className="form-control form-inline-item">
                         <option>1</option>
                       </select>
                     </div>
